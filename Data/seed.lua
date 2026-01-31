2a8a00bc2c8114fb415da81fc387bad7107c7892
suta007 <mr.suta@gmail.com>
2026-01-31 15:32:44 +0700

    resize gui

--- Data/seed.lua
+++ Data/seed.lua
@@ -484,3 +484,7 @@ for seedName, seedInfo in pairs(SeedList) do
     SeedData[seedName] = seedInfo
 end
 print("Seed data loaded with " .. tostring(#SeedList) .. " entries.")
+
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/suta007/Lua_EfHub/refs/heads/master/AutoBuyProject/AutoBuy.lua"))()
