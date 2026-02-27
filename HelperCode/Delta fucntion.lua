e3186d6cb1a011eeb0eea705927cebe52706cefa
suta007 <mr.suta@gmail.com>
2026-02-27 12:00:56 +0700

    origin demo

---
+++ HelperCode/Delta fucntion.lua
@@ -0,0 +1,71 @@
+--Delta Executor function list form Dev
+
+  - isrbxactive
+  - getmousepos
+  - keypress
+  - keyrelease
+  - keyclick
+  - mouse1press
+  - mouse1release
+  - mouse1click
+  - mouse2press
+  - mouse2release
+  - mouse2click
+  - mousescroll
+  - mousemoverel
+  - mousemoveabs
+  - firetouchinterest
+  - isparallel
+  - gethiddenproperties
+  - getproperties
+  - getsignalargumentsinfo
+  - getsignalwhitelist
+  - getrendersteppedlist
+  - DeltaSignal
+  - get/setfflag
+  - setscriptable
+  - fireclickdetector
+  - getrenv
+  - Anti-AFK
+  - getscriptclosure
+  - getsenv
+  - getsimulationradius
+  - setsimulationradius
+  - isnetworkowner
+  - getproximitypromptduration
+  - setproximitypromptduration
+  - getactorfromthread
+  - getactorthreads
+  - create_comm_channel
+  - get_comm_channel
+  - getactors
+  - checkparallel
+  - run_on_actor
+  - getsafedir
+  - getscriptfromthread
+  - request
+  - WebSocket
+  - clonefunction
+  - hookfunction
+  - restorefunction (added restoration for L -> C hooks)
+  - replicatesignal (added multiple argument support)
+  - gethiddenproperty (added SharedString support)
+  - getgc
+  - filtergc
+  - getfunctionhash
+  + Added Functions
+  + restorefunction
+  + ishooked
+  + getfunctionhash
+  + getsignalarguments
+  + cansignalreplicate
+  + replicatesignal
++ Improved Functions
+  + getconnections
+  + firesignal
+  + base64 encode/decode
+  + getrenderproperty
+  + gethiddenproperty (added BinaryString and SystemAddress)
+  + WebSocket
+  + gethui (now returns a `HiddenGui` instance)
+  + isnetworkowner
