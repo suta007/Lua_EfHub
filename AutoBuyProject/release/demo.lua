b6e58a564c561b64e1eb16aa86bf81f56e0a26f6
suta007 <mr.suta@gmail.com>
2026-02-28 21:14:11 +0700

    add Shovel system

--- AutoBuyProject/release/demo.lua
+++ AutoBuyProject/release/demo.lua
@@ -3603,7 +3603,7 @@ SellPetEgg = function()
 	end
 end
 
-ShovelPlant = function()
+--[[ ShovelPlant = function()
 	if not Options.tgAutoPlantShovel.Value then
 		return
 	end
@@ -3624,7 +3624,7 @@ ShovelPlant = function()
 			end
 		end
 	end
-end
+end ]]
 
 --End of Main Function
 
